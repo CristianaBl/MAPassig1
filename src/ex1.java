eacd752341257fb5579fb9061814eb3db8c18b4f
Cristiana Bleoca <cristiana.t3odora@gmail.com>
2024-10-13 13:51:48 +0300

    ex1a

---
+++ src/ex1.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ex1 {
+
+    public static int roundGrade(int grade) {
+        if (grade < 38) {
+            return grade;
+        }
+        int multiple5 = ((grade / 5) + 1) * 5;
+        if (multiple5-grade < 3) {
+            return multiple5;
+        }
+        return grade;
+    }
+//a)
+    public static List<Integer> failedGrades(int[] grades) {
+        List<Integer> failingGrades = new ArrayList<>();
+
+        for (int grade:grades) {
+            int roundedGrade=roundGrade(grade);
+            if (roundedGrade<40) {
+                failingGrades.add(roundedGrade);
+            }
+        }
+        return failingGrades;
+    }
+
+    public static void main(String[] args) {
+        int[] grades = {84, 29, 57, 38, 33};
+        List<Integer> failingGrades = failedGrades(grades);
+
+        System.out.println("Failed grades: " + failingGrades);
+    }
+
+
+}
+
