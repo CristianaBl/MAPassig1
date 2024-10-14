025ba056d7333395154c7e701af9272f8ec4ad2c
Cristiana Bleoca <cristiana.t3odora@gmail.com>
2024-10-14 15:26:52 +0300

    ex2b

--- src/ex2.java
+++ src/ex2.java
@@ -10,9 +10,24 @@ public class ex2 {
         }
         return max;
     }
+    //b)
+
+    public static int minValue(int[] numbers) {
+        int min =Integer.MAX_VALUE;
+        for (int number : numbers) {
+            if (number < min) {
+                min = number;
+            }
+        }
+        return min;
+    }
     public static void main(String[] args) {
-        int[] numbers = {-4, -8, -3, -10, -17,-3, -88};
+        int[] numbers = {4, 8, 3, 10, 17,3, 88};
+
         int max = maxValue(numbers);
         System.out.println("Max Value is: " + max);
+
+        int min = minValue(numbers);
+        System.out.println("Min Value is: " + min);
     }
 }
